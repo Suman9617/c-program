141368852cb979d28093c573f5c33b1d4c64e2eb
Suman9617 <www.sumanmondal9617@gmail.com>
2024-09-27 11:51:00 +0530

    Add files via upload

---
+++ swap ab 3.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+  int a,b,c;
+  printf("Enter the value a \n");
+  scanf("%d ",&a);
+  printf("Enter the value b\n");
+ scanf("%d",&b);
+  c=a;
+  a=b;
+  b=c;
+   printf("Enter the value %d and %d",a,b);
+   return 0;
+   
+  
+}
