cba6a253baa11882cf4528d68e5cc1daaccead1e
Suman9617 <www.sumanmondal9617@gmail.com>
2025-01-18 21:34:22 +0530

    Add files via upload

---
+++ problem2.c
@@ -0,0 +1,8 @@
+# include<stdio.h>
+int main(){
+    int r=9;
+    int h=7;
+    printf("area of the circle %.2f\n", r*r*3.14);
+    printf("volume of the cilinder %.2f\n", 3.14*r*h);
+    return 0;
+}
