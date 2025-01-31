44933c94617ea8ce258c84a88f0d93b56a16c98a
Suman9617 <www.sumanmondal9617@gmail.com>
2025-01-31 21:53:03 +0530

    Add files via upload

---
+++ program_001.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int function(int arr[],int size){
+    
+}
+int main(){
+   int n,res;
+   scanf("%d",&n);
+   int arr[n];
+   for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+   } 
+   res=function(arr,n);
+
+  return 0;
+}
