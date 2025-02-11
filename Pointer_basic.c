6cb7a06d829aedf4aad7793375e09a9366ff4a35
Suman9617 <www.sumanmondal9617@gmail.com>
2025-02-11 21:46:46 +0530

    Add files via upload

---
+++ Pointer_basic.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10, b = 9, c;
+    int *p = &a, *q = &b;
+    printf("The value of a=%d\n", *p);
+    printf("The value of b=%d", *q);
+
+    return 0;
+}
